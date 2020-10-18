9f392a77036eaa1b38bd1fb317ceba05ca58867b
Steve Kizer <kizerste@asu.edu>
2020-10-17 17:00:36 -0700

    TASK 48: Creates an initial script to import general standards into the database.

---
+++ vue-firebase-app/public/importStandards.js
@@ -0,0 +1,23 @@
+import {initializeApp} from 'firebase';
+
+const app = initializeApp({
+    apiKey: "AIzaSyCfqZoAkpuSBfp9n9sN3RCyHPV5wmA6cxs",
+    authDomain: "ser401-project-37.firebaseapp.com",
+    databaseURL: "https://ser401-project-37.firebaseio.com",
+    projectId: "ser401-project-37",
+    storageBucket: "ser401-project-37.appspot.com",
+    messagingSenderId: "46311260060",
+    appId: "1:46311260060:web:1e381fb482afc30c955259"
+  });
+
+const db = app.database();
+
+db.collection("GeneralStandards").add({
+  title: "Course Overview and Introduction"
+})
+.then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+  console.error("Error adding document: ", error);
+});
