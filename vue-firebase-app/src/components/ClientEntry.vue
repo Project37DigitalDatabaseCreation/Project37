f499f6d69fe8cc5bfa9330c8254e5c44ac850716
Andrew Massart <amassart@asu.edu>
2021-01-17 14:47:45 -0500

    Added Client entry page without dropdown menu or routing

---
+++ vue-firebase-app/src/components/ClientEntry.vue
@@ -0,0 +1,87 @@
+<!--
+* ClientEntry.vue
+*
+* Description: Provides the necessary fields to allow an admin user
+* to create and edit a client for each organization
+*
+the following fields: First Name, Last Name, Email, and Organization
+
+Organization is a drop down: relies on project being created.
+-->
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Clients:</div>
+          <div class="card-body">
+            <form>
+              <div class="form-group row">
+                <label class="col-md-4 col-form-label text-md-right"
+                  >First Name</label
+                >
+                <div class="col-md-6">
+                  <input
+                    type="text"
+                    required
+                    placeholder="First Name"
+                    v-model="firstName"
+                  />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-md-4 col-form-label text-md-right"
+                  >Last Name</label
+                >
+                <div class="col-md-6">
+                  <input
+                    type="text"
+                    required
+                    placeholder="Last Name"
+                    v-model="lastName"
+                  />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-md-4 col-form-label text-md-right"
+                  >Email</label
+                >
+                <div class="col-md-6">
+                  <input
+                    type="email"
+                    required
+                    placeholder="Email"
+                    v-model="email"
+                  />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-md-4 col-form-label text-md-right"
+                  >Organization</label
+                >
+                <b-form-select
+                  v-model="selected"
+                  :options="options"
+                ></b-form-select>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        selected: null,
+        options: [
+          { value: null, text: 'Select and Organization' },
+          { value: 'a', text: 'ASU' },
+        ],
+      }
+    },
+  }
+</script>
