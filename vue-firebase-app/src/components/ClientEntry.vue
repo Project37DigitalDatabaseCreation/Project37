c8e4fa0ff4593fd01edf9c82fd4daff743df8ba8
Andrew Massart <amassart@asu.edu>
2021-01-23 01:46:05 -0500

    Submitting form now creates client document in firebase

--- vue-firebase-app/src/components/ClientEntry.vue
+++ vue-firebase-app/src/components/ClientEntry.vue
@@ -17,14 +17,19 @@ Organization is a drop down: relies on project being created.
             Add Clients:
           </div>
           <div class="card-body">
-            <form @submit.prevent="">
+            <form @submit.prevent="handleSubmit">
               <div class="form-group row">
                 <label class="col-md-4 col-form-label text-md-right"
                   >First Name</label
                 >
 
                 <div class="col-md-6">
-                  <input type="text" required placeholder="First Name" />
+                  <input
+                    type="text"
+                    required
+                    placeholder="First Name"
+                    v-model="firstName"
+                  />
                 </div>
               </div>
 
@@ -34,7 +39,12 @@ Organization is a drop down: relies on project being created.
                 >
 
                 <div class="col-md-6">
-                  <input type="text" required placeholder="Last Name" />
+                  <input
+                    type="text"
+                    required
+                    placeholder="Last Name"
+                    v-model="lastName"
+                  />
                 </div>
               </div>
 
@@ -44,7 +54,12 @@ Organization is a drop down: relies on project being created.
                 >
 
                 <div class="col-md-6">
-                  <input type="email" required placeholder="Email" />
+                  <input
+                    type="email"
+                    required
+                    placeholder="Email"
+                    v-model="email"
+                  />
                 </div>
               </div>
 
@@ -54,7 +69,7 @@ Organization is a drop down: relies on project being created.
                 >
 
                 <div class="col-md-6">
-                  <select required>
+                  <select required v-model="organization">
                     <option
                       v-for="organization in organizations"
                       v-bind:key="organization.title"
@@ -81,15 +96,49 @@ Organization is a drop down: relies on project being created.
 </template>
 
 <script>
+  import firebase from 'firebase'
+  import 'firebase/firestore'
   import getOrganizations from '../composables/getOrganizations'
+  import { ref } from 'vue'
 
   export default {
     setup() {
+      const firstName = ref('')
+      const lastName = ref('')
+      const email = ref('')
+      const organization = ref('')
+
       const { organizations, error, load } = getOrganizations()
 
+      // loads the current organizations from firebase for the dropdown menu
       load()
 
-      return { organizations, error }
+      // creates the client document in firebase on submitting the form
+      const handleSubmit = async () => {
+        const client = {
+          firstName: firstName.value,
+          lastName: lastName.value,
+          email: email.value,
+          organization: organization.value,
+        }
+
+        const res = await firebase
+          .firestore()
+          .collection('Clients')
+          .add(client)
+
+        console.log(res)
+      }
+
+      return {
+        organizations,
+        error,
+        handleSubmit,
+        firstName,
+        lastName,
+        email,
+        organization,
+      }
     },
   }
 </script>
