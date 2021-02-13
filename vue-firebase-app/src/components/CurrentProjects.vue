144aebb44b90d9a806cc7edb17c26402a9dec673
Armando <armandominorjr@gmail.com>
2021-02-13 14:27:24 -0700

    User can now add multiple clients to a new project. This information is also refelcted in the CurrentProjects and ViewProject  views.

--- vue-firebase-app/src/components/CurrentProjects.vue
+++ vue-firebase-app/src/components/CurrentProjects.vue
@@ -9,37 +9,35 @@
 <template>
     <!-- Card which displays all  -->
     <div id="project-info" class="container">
-      <div style="position:fixed; left:200px; width:90%;">
-        <div class="justify-content-center">
-          <div class="col-md-10" style="padding:0 !important;">
-            <div class="card">
-            <div class="card-header text-center" style="font-size: 1.5em">Current Projects</div>
-            <table>
-              <thead>
-              <tr>
-                <th>Project</th>
-                <th>Clients</th>
-                <th>Reviews</th>
-                <th>Status</th>
-                <th>Organization</th>
-              </tr>
-              </thead>
-
-              <tbody>
+    <div style="position:fixed; left:200px; width:90%;">
+    <div class="justify-content-center">
+    <div class="col-md-10" style="padding:0 !important;">
+    <div class="card">
+    <div class="card-header text-center" style="font-size: 1.5em">Current Projects</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Project</th>
+            <th>Clients</th>
+            <th>Reviews</th>
+            <th>Status</th>
+            <th>Organization</th>
+            </tr>
+            </thead>
+          <tbody>
               <tr v-for="project in projects" :key="project" v-on:click="clickProject(project)">
                 <td>{{project.title}}</td>
-                <td>{{project.clients}}</td>
+                <td>{{project.clients.join(', ')}}</td>
                 <td>{{project.num_reviews}}</td>
                 <td>{{project.status}}</td>
                 <td>{{project.organization}}</td>
               </tr>
-              </tbody>
-              
-            </table>
-            </div>
-          </div>
-        </div>
-      </div>
+          </tbody>
+      </table>
+    </div>
+    </div>
+    </div>
+    </div>
     </div>
 </template>
 
@@ -60,7 +58,7 @@ export default {
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-           this.projects.push({
+            this.projects.push({
               id: doc.id,
               title: doc.data().title,
               status: doc.data().status,
@@ -71,21 +69,20 @@ export default {
               clients: doc.data().clients
             });
           });
-          return projects
+        return projects
         })
         .catch((error) => {
           console.log("Error retrieving documents: ", error);
         });
-    },
-    clickProject(project){
+      },
+    clickProject(project) {
       console.log("clickList fired with " + project.title);
       this.$router.push({
-    name: 'ViewProject',
-    params: {
-        project: project.title
-    }
-});
-
+        name: 'ViewProject',
+        params: {
+          project: project.title
+        }
+      });
     }
   },
   mounted() {
