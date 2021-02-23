fec7c2360e84572b93a6fea6717e8b44cdf93290
Steve Kizer <kizerste@asu.edu>
2021-02-22 17:46:16 -0700

    US#241: Attempts to add a cloud function to update a counter.

--- vue-firebase-app/functions/index.js
+++ vue-firebase-app/functions/index.js
@@ -1,9 +1,54 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.reviewsCounter = functions.database.ref('Reviews/{reviewId}')
+    .onWrite(async (change, context) => {
+
+        try {
+
+            if (change.after.exists()) {
+                if (change.before.exists()) {
+
+                    const valObjBefore = change.before.val();
+                    const valObjAfter = change.after.val();
+
+                    const nbrReviewsBefore = Object.keys(valObjBefore).length;
+                    const nbrReviewsAfter = Object.keys(valObjAfter).length;
+
+                    if (nbrReviewsBefore !== nbrReviewsAfter) {
+
+                        //We update the reviewCounter node
+                        const reviewCounterRef = change.after.ref.parent.child('reviewsCounter');
+                        await reviewCounterRef.transaction(() => {
+                            return nbrReviewsAfter;
+                        });
+                        return null;
+
+                    } else {
+                        //No need to update the reviewsCounter node
+                        return null;
+                    }
+
+                } else {                    
+                    const reviewCounterRef = change.before.ref.parent.child('reviewsCounter');
+                    await reviewCounterRef.transaction(() => {
+                        return 1;
+                    });
+                    return null;
+                }
+
+            } else {
+                const userJobsCounterRef = change.before.ref.parent.child('reviewsCounter');
+                await userJobsCounterRef.transaction(() => {
+                    return 0;
+                });
+                return null;
+            }
+
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+
+    });
