8d636ff36de851d39530b435fef53b03483b4397
Isaac Landis <ilandis@asu.edu>
2021-03-03 19:29:55 -0800

    Task 249 - Refactor Review body to use Scores

--- vue-firebase-app/src/components/Review.vue
+++ vue-firebase-app/src/components/Review.vue
@@ -4,6 +4,10 @@
         <ReviewForm v-if="ready" :currentLink="currentLink" :edit="review ? true : false"
             :review="selectedReview" :scores="scores">
         </ReviewForm>
+        <div v-else
+            style="display:flex; justify-content:center; align-items:center; width:100%;">
+            <div class="loader"></div>
+        </div>
     </div>
 </template>
 <script>
@@ -37,34 +41,49 @@ export default {
         console.log('REVIEW', this.review)
         //  If we have a review string, we are editing so fetch our scores
         if (this.review) {
+            //  We have to grab the review that the id points to
+            const rev = firebase.firestore().collection('Reviews').doc(this.review)
+
             //  Get our scores
-            let temp = await firebase
+            const temp = await firebase
                 .firestore()
                 .collection('Scores')
-                .where('review_ref', '==', `/Reviews/${this.review}`)
+                .where('review_ref', '==', rev)
                 .get()
 
             //  We also have to get our review
-            let review = await firebase.firestore().collection('Reviews').doc(this.review)
+            const review = await firebase
+                .firestore()
+                .collection('Reviews')
+                .doc(this.review)
+
             //  If the review is set, set our selected, else just null
             this.selectedReview = review || null
 
             //  If these scores exist, use it
             if (temp) {
                 //  Get each of our score documents
-                temp.snapshot.docs.forEach(async (doc) => {
+                for (let i = 0; i < temp.docs.length; i++) {
+                    //  Get the current document
+                    let doc = temp.docs[i]
                     //  Our score
                     const score = doc.data()
                     //  We also have to get our standard
-                    const std = score.standard_ref.get()
+                    const std = await score.standard_ref.get()
                     //  Add the standard to our score
                     score.standard = std.data()
                     score.standard.id = std.id
+
+                    //  Get the general standard on this standard
+                    const gen = await score.standard.general_standard_ref.get()
+                    //  Add the general standard to our standard
+                    score.standard.generalStandard = gen.data()
+                    score.standard.generalStandard.id = gen.id
                     //  Append our id
                     score.id = doc.id
                     //  Push into our scores array
                     this.scores.push(score)
-                })
+                }
                 console.log('FINISHED', this.scores)
                 //  Signal we are ready to go
                 this.ready = true
@@ -116,8 +135,27 @@ export default {
     },
     watch: {
         sortedStandards(val) {
+            //  Create our template once we have standards AND there's no scores/review (create)
             if (val && !this.scores.length && !this.review) this.createTemplate()
         }
     }
 }
 </script>
+<style scoped>
+.loader {
+    border: 16px solid #f3f3f3; /* Light grey */
+    border-top: 16px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+}
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
