5a0c6d6c96b11f3bb0831a5735669fe57b95b742
Steve Kizer <kizerste@asu.edu>
2021-03-04 17:03:31 -0700

    US#241: Implements all necessary event handlers for aggregating reviews by status.

--- vue-firebase-app/functions/index.js
+++ vue-firebase-app/functions/index.js
@@ -26,73 +26,33 @@ exports.createReviewer = functions.https.onCall((request, context) => {
         throw new functions.https.HttpsError('internal', error.message)
       });
   });
-  
-  
-exports.reviewStatusCounter = functions.firestore.document('Reviews/{reviewId}')
-    .onWrite((change, context) => {
-        console.log("Before: " + change.before.exists);
-        console.log("After: " + change.after.exists);
+
+exports.reviewCreateStatusCounter = functions.firestore.document('Reviews/{reviewId}')
+    .onCreate(async (snap, context) => {
         try {       
             const col = db.collection("Stats");
             const doc = db.doc('Stats/ReviewsByStatus');
             const arrayUnion = admin.firestore.FieldValue.arrayUnion;
-            const arrayRemove = admin.firestore.FieldValue.arrayRemove;
-                         
-            if(change.after.exists && change.before.exists) {
-                // the review has been updated, check if status changed
-                let reviewBefore = change.before.data();
-                let reviewAfter = change.after.data();
+            const increment = admin.firestore.FieldValue.increment(1);
 
-                reviewBefore.id = change.before.id;
-                reviewAfter.id = change.after.id;
-
-                if(reviewBefore.status !== reviewAfter.status) {                
-                    if(reviewBefore.status == "New" && reviewAfter.status == "In-Progress") {
-                        col.where("NewReviews", "array-contains", reviewBefore.id).get()
-                        .then(result => {
-                            if (result.size > 0) {
-                                return doc.update({
-                                    NewReviews: arrayRemove(reviewBefore.id),
-                                    InProgressReviews: arrayUnion(reviewBefore.id)
-                                });
-                            } else {
-                                return null;
-                            }
-                        });
-                    }
-
-                    if(reviewBefore.status == "In-Progress" && reviewAfter.status == "Complete") {
-                        col.where("InProgressReviews", "array-contains", reviewBefore.id).get()
-                        .then(result => {
-                            if(result.size > 0) {
-                                return doc.update({
-                                    InProgressReviews: arrayRemove(reviewBefore.id),
-                                    CompleteReviews: arrayUnion(reviewBefore.id)
-                                });
-                            } else {
-                                return null;
-                            }
-                        });
-                    }
+            let newReview = snap.data();
+            let docId = snap.id;
+                
+            // handle a new review
+            if(newReview.status == "New") {
+                let result = await col.where("NewReviews", "array-contains", docId).get();
+                    
+                if(result.size == 0) {
+                    return doc.set({
+                        NewReviewsCount: increment,
+                        NewReviews: arrayUnion(docId)
+                    },
+                    {
+                        merge: true
+                    });
                 } else {
                     return null;
                 }
-            } else if(change.after.exists && !change.before.exists) {
-                let reviewAfter = change.after.data();
-                let docId = change.after.id;
-
-                if(reviewAfter.status == "New") {
-                    col.where("NewReviews", "array-contains", docId).get()
-                    .then(result => {
-                        if(result.size == 0) {
-                            return doc.set({
-                                NewReviews: arrayUnion(docId)
-                            });
-                        } else {
-                            return null;
-                        }
-                    });
-                }                
             } else {
                 return null;
             }
@@ -100,4 +60,136 @@ exports.reviewStatusCounter = functions.firestore.document('Reviews/{reviewId}')
             console.log(error);
             return null;
         }
-    });
+    }
+);
+
+exports.reviewUpdateStatusCounter = functions.firestore.document('Reviews/{reviewId}')
+    .onUpdate(async (change, context) => {
+        try {       
+            const col = db.collection("Stats");
+            const doc = db.doc('Stats/ReviewsByStatus');
+            const arrayUnion = admin.firestore.FieldValue.arrayUnion;
+            const arrayRemove = admin.firestore.FieldValue.arrayRemove;
+            const increment = admin.firestore.FieldValue.increment(1);
+            const decrement = admin.firestore.FieldValue.increment(-1);
+                         
+            // the review has been updated, check if status changed
+            let reviewBefore = change.before.data();
+            let reviewAfter = change.after.data();
+
+            reviewBefore.id = change.before.id;
+            reviewAfter.id = change.after.id;
+            
+            if(reviewBefore.status !== reviewAfter.status) {  
+                // handle review going from New to In-Progress              
+                if(reviewBefore.status == "New" && reviewAfter.status == "In-Progress") {
+                    let result = await col.where("NewReviews", "array-contains", reviewBefore.id).get();
+
+                    if (result.size > 0) {
+                        return doc.set({
+                            NewReviewsCount: decrement,
+                            InProgressReviewsCount: increment,                                                                    
+                            NewReviews: arrayRemove(reviewBefore.id),
+                            InProgressReviews: arrayUnion(reviewBefore.id)
+                        },
+                        {
+                            merge: true
+                        });                                
+                    } else {
+                        return null;
+                    }
+                }
+
+                // handle review going from In-Progress to Complete
+                if(reviewBefore.status == "In-Progress" && reviewAfter.status == "Complete") {
+                    let result = await col.where("InProgressReviews", "array-contains", reviewBefore.id).get();
+
+                    if(result.size > 0) {
+                        doc.set({
+                            InProgressReviewsCount: decrement,
+                            CompletedReviewsCount: increment,
+                            InProgressReviews: arrayRemove(reviewBefore.id),
+                            CompletedReviews: arrayUnion(reviewBefore.id)
+                        },
+                        {
+                            merge: true
+                        });
+                    } else {
+                        return null;
+                    }
+                }
+
+                return null;
+            } else {
+                return null;
+            }
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
+);
+
+exports.reviewDeleteStatusCounter = functions.firestore.document('Reviews/{reviewId}')
+    .onDelete(async (snap, context) => {
+        try {       
+            const col = db.collection("Stats");
+            const doc = db.doc('Stats/ReviewsByStatus');
+            const arrayRemove = admin.firestore.FieldValue.arrayRemove;
+            const decrement = admin.firestore.FieldValue.increment(-1);
+                         
+            // a review must have been deleted
+            let deletedReview = snap.data();
+            let docId = snap.id;
+
+            // each possible status needs to be handled
+            if(deletedReview.status == "New") {          
+                let result = await col.where("NewReviews", "array-contains", docId).get();
+
+                if(result.size > 0) {
+                    return doc.set({
+                        NewReviewsCount: decrement,
+                        NewReviews: arrayRemove(docId)
+                    },
+                    {
+                        merge: true
+                    });
+                } else {
+                    return null;h
+                }
+            } else if(deletedReview.status == "In-Progress") {
+                let result = await col.where("InProgressReviews", "array-contains", docId).get();
+                                        
+                if(result.size > 0) {
+                    return doc.set({
+                        InProgressReviewsCount: decrement,
+                        InProgressReviews: arrayRemove(docId)
+                    },
+                    {
+                        merge: true
+                    });
+                } else {
+                    return null;
+                }                  
+            } else {
+                let result = await col.where("CompletedReviews", "array-contains", docId).get();
+
+                if(result.size > 0) {
+                    return doc.set({
+                        CompletedReviewsCount: decrement,
+                        CompletedReviews: arrayRemove(docId)
+                    },
+                    {
+                        merge: true
+                    });
+                }
+                else {
+                    return null;
+                }             
+            }
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
+);
