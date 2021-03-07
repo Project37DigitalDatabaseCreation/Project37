638a0f756612fcbcd8d9cb616d0933c31d786b23
Ben McElyea <bmcelyea@gmail.com>
2021-03-06 19:08:00 -0500

    Fixed issues in createreviewer

--- vue-firebase-app/functions/index.js
+++ vue-firebase-app/functions/index.js
@@ -10,7 +10,6 @@ const db = admin.firestore();
 // Date: February 2021
 // createReviewer - A Cloud function to add users to the Firebase authentication DB 
 // To update this function you must run the command firebase deploy --only functions 
-
 exports.createReviewer = functions.https.onCall((request, context) => {
   console.log(request.email)
 
@@ -29,13 +28,14 @@ exports.createReviewer = functions.https.onCall((request, context) => {
 }});
 
 
+
 // Function Author: Ben McElyea
 // Date: February 2021
 // deleteReviewer - A Cloud function to delete users to the Firebase authentication DB 
 // To update this function you must run the command firebase deploy --only functions 
 
 exports.deleteReviewer = functions.https.onCall((request, context) => {
-  console.log("Deleting user" + request.email)
+    console.log("Deleting user" + request.email)
 
   //Only allow authenticated users to call this function. 
   if (!context.auth) {
@@ -52,7 +52,7 @@ exports.deleteReviewer = functions.https.onCall((request, context) => {
 
 exports.reviewCreateStatusCounter = functions.firestore.document('Reviews/{reviewId}')
     .onCreate(async (snap, context) => {
-        try {       
+        try {
             const col = db.collection("Stats");
             const doc = db.doc('Stats/ReviewsByStatus');
             const arrayUnion = admin.firestore.FieldValue.arrayUnion;
@@ -60,7 +60,7 @@ exports.reviewCreateStatusCounter = functions.firestore.document('Reviews/{revie
 
             let newReview = snap.data();
             let docId = snap.id;
-                
+
             // handle a new review
             if(newReview.status === "New") {
                 let result = await col.where("NewReviews", "array-contains", docId).get();
@@ -70,9 +70,9 @@ exports.reviewCreateStatusCounter = functions.firestore.document('Reviews/{revie
                         NewReviewsCount: increment,
                         NewReviews: arrayUnion(docId)
                     },
-                    {
-                        merge: true
-                    });
+                        {
+                            merge: true
+                        });
                 } else {
                     return null;
                 }
@@ -84,26 +84,26 @@ exports.reviewCreateStatusCounter = functions.firestore.document('Reviews/{revie
             return null;
         }
     }
-);
+    );
 
 exports.reviewUpdateStatusCounter = functions.firestore.document('Reviews/{reviewId}')
     .onUpdate(async (change, context) => {
-        try {       
+        try {
             const col = db.collection("Stats");
             const doc = db.doc('Stats/ReviewsByStatus');
             const arrayUnion = admin.firestore.FieldValue.arrayUnion;
             const arrayRemove = admin.firestore.FieldValue.arrayRemove;
             const increment = admin.firestore.FieldValue.increment(1);
             const decrement = admin.firestore.FieldValue.increment(-1);
-                         
+
             // the review has been updated, check if status changed
             let reviewBefore = change.before.data();
             let reviewAfter = change.after.data();
 
             reviewBefore.id = change.before.id;
             reviewAfter.id = change.after.id;
-            
-            if(reviewBefore.status !== reviewAfter.status) {  
+
+            if (reviewBefore.status !== reviewAfter.status) {
                 // handle review going from New to In-Progress              
                 if(reviewBefore.status === "New" && reviewAfter.status === "In-Progress") {
                     let result = await col.where("NewReviews", "array-contains", reviewBefore.id).get();
@@ -111,13 +111,13 @@ exports.reviewUpdateStatusCounter = functions.firestore.document('Reviews/{revie
                     if (result.size > 0) {
                         return doc.set({
                             NewReviewsCount: decrement,
-                            InProgressReviewsCount: increment,                                                                    
+                            InProgressReviewsCount: increment,
                             NewReviews: arrayRemove(reviewBefore.id),
                             InProgressReviews: arrayUnion(reviewBefore.id)
                         },
-                        {
-                            merge: true
-                        });                                
+                            {
+                                merge: true
+                            });
                     } else {
                         return null;
                     }
@@ -127,16 +127,16 @@ exports.reviewUpdateStatusCounter = functions.firestore.document('Reviews/{revie
                 if(reviewBefore.status === "In-Progress" && reviewAfter.status === "Complete") {
                     let result = await col.where("InProgressReviews", "array-contains", reviewBefore.id).get();
 
-                    if(result.size > 0) {
+                    if (result.size > 0) {
                         doc.set({
                             InProgressReviewsCount: decrement,
                             CompletedReviewsCount: increment,
                             InProgressReviews: arrayRemove(reviewBefore.id),
                             CompletedReviews: arrayUnion(reviewBefore.id)
                         },
-                        {
-                            merge: true
-                        });
+                            {
+                                merge: true
+                            });
                     } else {
                         return null;
                     }
@@ -151,16 +151,16 @@ exports.reviewUpdateStatusCounter = functions.firestore.document('Reviews/{revie
             return null;
         }
     }
-);
+    );
 
 exports.reviewDeleteStatusCounter = functions.firestore.document('Reviews/{reviewId}')
     .onDelete(async (snap, context) => {
-        try {       
+        try {
             const col = db.collection("Stats");
             const doc = db.doc('Stats/ReviewsByStatus');
             const arrayRemove = admin.firestore.FieldValue.arrayRemove;
             const decrement = admin.firestore.FieldValue.increment(-1);
-                         
+
             // a review must have been deleted
             let deletedReview = snap.data();
             let docId = snap.id;
@@ -169,53 +169,53 @@ exports.reviewDeleteStatusCounter = functions.firestore.document('Reviews/{revie
             if(deletedReview.status === "New") {          
                 let result = await col.where("NewReviews", "array-contains", docId).get();
 
-                if(result.size > 0) {
+                if (result.size > 0) {
                     return doc.set({
                         NewReviewsCount: decrement,
                         NewReviews: arrayRemove(docId)
                     },
-                    {
-                        merge: true
-                    });
+                        {
+                            merge: true
+                        });
                 } else {
                     return null;
                 }
             } else if(deletedReview.status === "In-Progress") {
                 let result = await col.where("InProgressReviews", "array-contains", docId).get();
-                                        
-                if(result.size > 0) {
+
+                if (result.size > 0) {
                     return doc.set({
                         InProgressReviewsCount: decrement,
                         InProgressReviews: arrayRemove(docId)
                     },
-                    {
-                        merge: true
-                    });
+                        {
+                            merge: true
+                        });
                 } else {
                     return null;
-                }                  
+                }
             } else {
                 let result = await col.where("CompletedReviews", "array-contains", docId).get();
 
-                if(result.size > 0) {
+                if (result.size > 0) {
                     return doc.set({
                         CompletedReviewsCount: decrement,
                         CompletedReviews: arrayRemove(docId)
                     },
-                    {
-                        merge: true
-                    });
+                        {
+                            merge: true
+                        });
                 }
                 else {
                     return null;
-                }             
+                }
             }
         } catch (error) {
             console.log(error);
             return null;
         }
     }
-);
+    );
 
 
 // Function Author: Ben McElyea
