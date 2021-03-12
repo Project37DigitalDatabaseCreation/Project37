2d408672d8303529d32588691694dfa5fc384e19
Isaac Landis <ilandis@asu.edu>
2021-03-11 18:37:47 -0800

    Task 282 - Admins can view invitations and accept the user to become a client

--- vue-firebase-app/src/components/ManageClients.vue
+++ vue-firebase-app/src/components/ManageClients.vue
@@ -6,124 +6,125 @@
 -->
 
 <template>
-  <div class="container">
-    <div class="container-layout">
-      <div class="col-md-10">
-      <div class="card">
-    <div class="card-header"> Manage Clients
-          <div style="float:right;">
-      <button @click="showModal = true" class="btn btn-primary btn-sm">
-        Add Clients
-      </button>
-      <client-modify-modal
-        v-if="showEditModal"
-        :update_client="update_client"
-        @close="showEditModal = false"
-      ></client-modify-modal>
+    <div class="container">
+        <div class="container-layout">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header"> Manage Clients
+                        <div style="float:right;">
+                            <button @click="goToInvitations"
+                                class="btn btn-primary btn-sm" style="margin-right:4px;">
+                                View Invitations
+                            </button>
+                            <button @click="showModal = true"
+                                class="btn btn-primary btn-sm">
+                                Add Clients
+                            </button>
+                            <client-modify-modal v-if="showEditModal"
+                                :update_client="update_client"
+                                @close="showEditModal = false"></client-modify-modal>
+                        </div>
+                    </div>
+                    <table class="table">
+                        <thead class="project-head">
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email Address</th>
+                                <th>Organization</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody class="project-body">
+                            <tr v-for="client in clients" :key="client.id">
+                                <td>{{ client.firstName }}</td>
+                                <td>{{ client.lastName }}</td>
+                                <td>{{ client.email }}</td>
+                                <td>{{ client.organization }}</td>
+                                <td>
+                                    <button
+                                        @click="modifyClient(client), (showEditModal = true)"
+                                        class="btn edit">
+                                        Modify
+                                    </button>
+                                </td>
+                                <td>
+                                    <button @click="handleDelete(client.id)"
+                                        class="btn delete">
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p class="mt-3">Current Page: {{ currentPage }}</p>
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-    <table class="table">
-      <thead class="project-head">
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email Address</th>
-          <th>Organization</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody class="project-body">
-        <tr v-for="client in clients" :key="client.id">
-          <td>{{ client.firstName }}</td>
-          <td>{{ client.lastName }}</td>
-          <td>{{ client.email }}</td>
-          <td>{{ client.organization }}</td>
-          <td>
-            <button
-              @click="modifyClient(client), (showEditModal = true)"
-              class="btn edit"
-            >
-              Modify
-            </button>
-          </td>
-          <td>
-            <button
-              @click="handleDelete(client.id)"
-              class="btn delete"
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <p class="mt-3">Current Page: {{ currentPage }}</p>
-    </div>
-    </div>
-    </div>
-  </div>
 
-  <!-- modal definition -->
-  <client-modal
-    v-if="showModal"
-    :new_client="new_client"
-    @close="showModal = false"
-  />
+    <!-- modal definition -->
+    <client-modal v-if="showModal" :new_client="new_client" @close="showModal = false" />
 </template>
 
 <script>
-  import modifyDocument from '../composables/modifyDocument'
-  import ClientModal from '../components/ClientModal'
-  import ClientModifyModal from '../components/ClientModifyModal'
-  import { ref, reactive } from 'vue'
-  import getCollection from '../composables/getCollection'
+import modifyDocument from '../composables/modifyDocument'
+import ClientModal from '../components/ClientModal'
+import ClientModifyModal from '../components/ClientModifyModal'
+import { ref, reactive } from 'vue'
+import getCollection from '../composables/getCollection'
 
-  export default {
+export default {
     components: { ClientModal, ClientModifyModal },
 
     setup() {
-      const showModal = ref(false)
-      const showEditModal = ref(false)
-      const currentPage = ref(1)
-      const { documents: clients, error } = getCollection('Clients')
-      const new_client = reactive({
-        firstName: '',
-        lastName: '',
-        email: '',
-        organization: '',
-      })
-      const update_client = reactive({})
+        const showModal = ref(false)
+        const showEditModal = ref(false)
+        const currentPage = ref(1)
+        const { documents: clients, error } = getCollection('Clients')
+        const new_client = reactive({
+            firstName: '',
+            lastName: '',
+            email: '',
+            organization: ''
+        })
+        const update_client = reactive({})
 
-      const modifyClient = (client) => {
-        let updates = {}
-        updates = Object.assign({}, client)
-        update_client.value = updates
-      }
-
-      const handleDelete = async (id) => {
-        if (confirm('Are you sure?')) {
-          modifyDocument('Clients', id).deleteDoc()
-        } else {
-          console.log('error')
+        const modifyClient = (client) => {
+            let updates = {}
+            updates = Object.assign({}, client)
+            update_client.value = updates
         }
-      }
 
-      return {
-        clients,
-        error,
-        currentPage,
-        showModal,
-        showEditModal,
-        handleDelete,
-        modifyClient,
-        ClientModal,
-        ClientModifyModal,
-        update_client,
-        new_client,
-      }
+        const handleDelete = async (id) => {
+            if (confirm('Are you sure?')) {
+                modifyDocument('Clients', id).deleteDoc()
+            } else {
+                console.log('error')
+            }
+        }
+
+        return {
+            clients,
+            error,
+            currentPage,
+            showModal,
+            showEditModal,
+            handleDelete,
+            modifyClient,
+            ClientModal,
+            ClientModifyModal,
+            update_client,
+            new_client
+        }
     },
-  }
+    methods: {
+        goToInvitations() {
+            this.$router.push('/invitations')
+        }
+    }
+}
 </script>
 
 <style scoped src="../assets/styles/styles.css"></style>
--- vue-firebase-app/src/components/ReviewForm.vue
+++ vue-firebase-app/src/components/ReviewForm.vue
@@ -409,21 +409,5 @@ export default {
     box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
         0 1px 5px 0 rgba(0, 0, 0, 0.12) !important;
 }
-.loader {
-    border: 16px solid #f3f3f3; /* Light grey */
-    border-top: 16px solid #3498db; /* Blue */
-    border-radius: 50%;
-    width: 120px;
-    height: 120px;
-    animation: spin 2s linear infinite;
-}
-@keyframes spin {
-    0% {
-        transform: rotate(0deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
-}
 /* TODO: Add in breakpoints for the width */
 </style>
