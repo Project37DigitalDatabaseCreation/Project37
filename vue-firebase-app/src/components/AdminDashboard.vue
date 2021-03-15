a577ec9409d864c968b923e694fb93f136779ee3
Steve Kizer <kizerste@asu.edu>
2021-03-15 15:31:01 -0700

    TASK#278: Splits out the reviews into separate tables based on status.

--- vue-firebase-app/src/components/AdminDashboard.vue
+++ vue-firebase-app/src/components/AdminDashboard.vue
@@ -7,6 +7,7 @@
 -->
 <template>
   <div class="container">
+    <span style="color: red">{{ error }}</span>
     <div class="row">
       <div class="col-sm-2">
         <div class="card">
@@ -49,12 +50,12 @@
         </div>
       </div>                  
     </div>
-    <h4 class="mt-4 text-center">Latest Reviews:</h4>
-    <span style="color: red">{{ error }}</span>
+    <h4 class="mt-4 text-center">In-Progress Reviews:</h4>    
     <table class="table mt-5">
       <thead>
         <tr>
           <th scope="col">Client</th>
+          <th scope="col">Course</th>
           <th scope="col">Reviewer</th>
           <th scope="col">Project</th>
           <th scope="col">Points</th>
@@ -63,8 +64,9 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="review in reviews" :key="review.id">
+        <tr v-for="review in in_progress_reviews" :key="review.id">
           <td>{{ review.org.title }}</td>
+          <td>{{ review.course_name }}</td>
           <td>{{ review.reviewer.lastName + ", " + review.reviewer.firstName }}</td>
           <td>{{ review.project.title }}</td>
           <td>{{ review.points }}</td>
@@ -73,6 +75,31 @@
         </tr>
       </tbody>          
     </table>
+    <h4 class="mt-4 text-center">Completed Reviews:</h4>
+    <table class="table mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Client</th>
+          <th scope="col">Course</th>
+          <th scope="col">Reviewer</th>
+          <th scope="col">Project</th>
+          <th scope="col">Points</th>
+          <th scope="col">Status</th>
+          <th scope="col">Review</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="review in completed_reviews" :key="review.id">
+          <td>{{ review.org.title }}</td>
+          <td>{{ review.course_name }}</td>
+          <td>{{ review.reviewer.lastName + ", " + review.reviewer.firstName }}</td>
+          <td>{{ review.project.title }}</td>
+          <td>{{ review.points }}</td>
+          <td>{{ review.met_status }}</td>
+          <td></td>
+        </tr>
+      </tbody>          
+    </table>    
   </div>
 </template>
 <script>
@@ -81,16 +108,82 @@ import firebase from "firebase";
 export default {
     data() {
     return {
-      reviews: [],
+      in_progress_reviews: [],
+      completed_reviews: [],
       standards: [],
       reviewStats: {},
       projectStats: {},
       error: null
     };
   },
-  async mounted() {
+  methods: {
+    addReviewToCollection(collection, review) {
+      let index = collection.push(review) - 1;   
+
+      review.project_ref.get()
+      .then(doc => {
+        let proj = doc.data();
+        
+        if (proj) {
+          proj.id = doc.id;
+          collection[index].project = proj;
+
+          proj.org_ref.get()
+          .then(doc => {
+            let org = doc.data();
+
+            if(org) {
+              org.id = doc.id;
+              collection[index].org = org; 
+            }           
+          }); 
+        }                              
+      });
+        
+      review.reviewer_ref.get()
+      .then(doc => {
+        let reviewer = doc.data();
+        
+        if(reviewer) {
+          reviewer.id = doc.id;
+          collection[index].reviewer = reviewer; 
+        }                                      
+      });        
+    },
+    tallyReviewScores(collection) {
+      const db = firebase.firestore();
+
+      for (let i = 0; i < collection.length; i++) {
+        let reviewRef = db.doc("/Reviews/" + collection[i].id);          
+        collection[i].points = 0;
+        collection[i].met_status = "Met";
+        
+        db.collection("Scores").where("review_ref", "==", reviewRef).get()
+        .then(result => {        
+          result.forEach(doc => {
+            let score = doc.data();
+            
+            if(score.standard_ref) {            
+              const scoreStandard = this.standards.find(function(standard) {
+                return standard.id === score.standard_ref.id;
+              });
+              
+              if (scoreStandard && score.met) {
+                collection[i].points += scoreStandard.points;
+              }
+              
+              if (scoreStandard && !score.met && scoreStandard.points == 3) {
+                collection[i].met_status = "Not Met";
+              }
+            }
+          });
+        });        
+      }
+    }
+  },
+  async mounted() {       
     const db = firebase.firestore();
-    
+
     db.doc('Stats/ReviewsByStatus').get()
     .then(result => {
       this.reviewStats = result.data();
@@ -113,29 +206,13 @@ export default {
         review.reviewer.firstName = "";
         review.project = {};
         review.project.title = "";
+        
+        if(review.status === "In-Progress")
+          this.addReviewToCollection(this.in_progress_reviews, review);
+        
+        if(review.status === "Complete")
+          this.addReviewToCollection(this.completed_reviews, review);
 
-        let index = this.reviews.push(review) - 1;   
-
-        review.project_ref.get()
-        .then(doc => {
-          let proj = doc.data();
-          proj.id = doc.id;
-          this.reviews[index].project = proj;
-
-          proj.org_ref.get()
-          .then(doc => {
-            let org = doc.data();
-            org.id = doc.id;
-            this.reviews[index].org = org;
-
-            review.reviewer_ref.get()
-            .then(doc => {
-              let reviewer = doc.data();
-              reviewer.id = doc.id;
-              this.reviews[index].reviewer = reviewer;                                       
-            });            
-          });              
-        });        
       })}).catch(err => { console.error(err) });
 
     let querySnapshot = await db.collection("Standards").get();
@@ -145,34 +222,10 @@ export default {
       standard.id = doc.id;
       standard.points = doc.data().points;
       this.standards.push(standard);
-    });        
-
-    for (let i = 0; i < this.reviews.length; i++) {
-      let reviewRef = db.doc("/Reviews/" + this.reviews[i].id);          
-      this.reviews[i].points = 0;
-      this.reviews[i].met_status = "Met";
-      
-      db.collection("Scores").where("review_ref", "==", reviewRef).get()
-      .then(result => {        
-        result.forEach(doc => {
-          let score = doc.data();
-          
-          if(score.standard_ref) {            
-            const scoreStandard = this.standards.find(function(standard) {
-              return standard.id === score.standard_ref.id;
-            });
-            
-            if (scoreStandard && score.met) {
-              this.reviews[i].points += scoreStandard.points;
-            }
-            
-            if (scoreStandard && !score.met && scoreStandard.points == 3) {
-              this.reviews[i].met_status = "Not Met";
-            }
-          }
-        });
-      });        
-    }
+    });      
+    
+    this.tallyReviewScores(this.in_progress_reviews);
+    this.tallyReviewScores(this.completed_reviews);
   }
 };
 </script>
