8cc1630eba5b8baa4f717768a94e40ba161a474f
Isaac Landis <ilandis@asu.edu>
2021-03-30 16:55:27 -0700

    Task 317 - Add total points to review cards

--- vue-firebase-app/src/components/ReviewsList.vue
+++ vue-firebase-app/src/components/ReviewsList.vue
@@ -1,57 +1,68 @@
 <template>
     <div class="container scrollcontainer">
-            <div class="col-md-10">
+        <div class="col-md-10">
             <div class="card">
-            <div class="card-header">All Reviews</div>
-            <table class="table">
-                <thead class="project-head">
-                    <tr>
-                        <th>Course Name</th>
-                        <th>Reviewer</th>
-                        <th>Status</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody class="project-body">
-                    <tr v-for="review in reviews" :key="review.id">
-                        <td>{{ review.course_name }}</td>
-                        <td>{{ reviewerName(review) }}
-                        </td>
-                        <td>{{ review.status }}</td>
-                        <td>
-                            <button class="btn edit" @click="goToEdit(review)"></button>
-                        </td>
-                        <!-- <td><button class="btn btn-primary" @click="populatePopOut(review)">Edit</button></td>
+                <div class="card-header">All Reviews</div>
+                <table class="table">
+                    <thead class="project-head">
+                        <tr>
+                            <th>Course Name</th>
+                            <th>Reviewer</th>
+                            <th>Status</th>
+                            <th>Points</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <div v-if="loading"
+                        style="display:flex; justify-content:center; align-items:center; width:100%;">
+                        <div class="loader"></div>
+                    </div>
+                    <tbody v-else class="project-body">
+                        <tr v-for="review in reviews" :key="review.id">
+                            <td>{{ review.course_name }}</td>
+                            <td>
+                                {{ reviewerName(review) }}
+                            </td>
+                            <td>{{ review.status }}</td>
+                            <td>{{ review.points }}</td>
+                            <td>
+                                <button class="btn btn-primary" @click="goToEdit(review)">
+                                    Edit Review
+                                </button>
+                            </td>
+                            <!-- <td><button class="btn btn-primary" @click="populatePopOut(review)">Edit</button></td>
                 <td><button class="btn btn-primary" @click="deleteReview(review)">Delete</button></td> -->
-                    </tr>
-                </tbody>
-            </table>
-            <span v-if="sortedStandards"
-                style="display:flex; justify-content:center; align-items:center; flex-direction:column;">
-                <button class="btn save" @click="goToCreate">
-                    Create Review
-                </button>
-            </span>
-            <span v-else
-                style="display:flex; justify-content:center; align-items:center; flex-direction:column;">
-                <button class="btn save" :disabled="!ready">
-                    Create Review
-                </button>
-                <div style="font-size:12px; margin-top:6px; color:grey;">We are getting
-                    the standards right now, you can create a review once
-                    they're all loaded.</div>
-            </span>
+                        </tr>
+                    </tbody>
+                </table>
+                <span v-if="sortedStandards"
+                    style="display:flex; justify-content:center; align-items:center; flex-direction:column;">
+                    <button class="btn btn-primary" @click="goToCreate">
+                        Create Review
+                    </button>
+                </span>
+                <span v-else
+                    style="display:flex; justify-content:center; align-items:center; flex-direction:column;">
+                    <button class="btn btn-primary" :disabled="!ready">
+                        Create Review
+                    </button>
+                    <div style="font-size:12px; margin-top:6px; color:grey;">We are
+                        getting
+                        the standards right now, you can create a review once
+                        they're all loaded.</div>
+                </span>
+            </div>
         </div>
-        </div>
-        <router-view></router-view>
+    </div>
+    <router-view></router-view>
     </div>
 </template>
 <script>
 export default {
-    mounted() {
-        this.$store.dispatch('fetchReviews')
-    },
     computed: {
+        loading() {
+            return this.$store.getters.loading
+        },
         reviews() {
             return this.$store.getters.reviews
         },
